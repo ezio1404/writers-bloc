52115b276d759e3ced67d32bc42622d30f8d72f4
EJ ANTON POTOT <zuming1404@gmail.com>
2021-12-01 05:24:46 +0800

    revisions

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
          */
         Spatie\Permission\PermissionServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        Yajra\DataTables\DataTablesServiceProvider::class,
+        Yajra\DataTables\ButtonsServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -230,6 +232,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
     ],
 
 ];
--- resources/views/components/teacher-navbar.blade.php
+++ resources/views/components/teacher-navbar.blade.php
@@ -50,6 +50,19 @@
                 <span> {{ __('Students') }}</span>
             </a>
         </li>
+        <li>
+            <a href="{{ route('teacher-report')}}"
+                class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-gray-200 @if(Request::path() === 'teacher/report') bg-gray-200 font-bold @else focus:bg-gray-200 @endif  focus:shadow-outline">
+                <span class="text-gray-600">
+                    <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                    </svg>
+                </span>
+                <span> {{ __('Reports') }}</span>
+            </a>
+        </li>
         {{-- <div class="border border-red-500">
             <ul>
                 <li class="text-red-500 font-bold">
